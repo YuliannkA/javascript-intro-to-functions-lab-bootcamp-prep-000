7c2714947bb6235a6cbf96c683ac3a3d558feb24
Yulia Drozdova <yuliannka@gmail.com>
2018-01-10 19:25:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,19 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if 
+  if string === string.toLowerCase()
   
+  
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
   
   
   
