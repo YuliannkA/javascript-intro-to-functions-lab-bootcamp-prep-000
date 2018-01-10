999fef608eef49e686acfdb7cae4848fcc451c3c
Yulia Drozdova <yuliannka@gmail.com>
2018-01-10 19:32:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string === string.toLowerCase()
+  if ( string === string.toLowerCase() ) {
+    return ""I can't hear you!""
+  }
   
   
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
